e9963a078fc5d01060a2a29aca25ae25f18bace0
Yinzhe Ma <31641047+yinzhema@users.noreply.github.com>
2021-04-20 23:35:21 -0400

    Update main.scala

--- src/main/scala/project_3/main.scala
+++ src/main/scala/project_3/main.scala
@@ -8,6 +8,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx._
 import org.apache.spark.storage.StorageLevel
 import org.apache.log4j.{Level, Logger}
+import scala.math.pow
 
 object main{
   val rootLogger = Logger.getRootLogger()
@@ -17,14 +18,73 @@ object main{
   Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
   def LubyMIS(g_in: Graph[Int, Int]): Graph[Int, Int] = {
-    while (remaining_vertices >= 1) {
-        // To Implement
+    var counter=0
+    var remaining_vertices=g_in.vertices.count
+    val undirectedGraph=Graph(g_in.vertices,g_in.reverse.edges.union(g_in.edges))
+    val random_num=scala.util.Random
+    var local_global=undirectedGraph.mapVertices[(Float,Boolean,Boolean,Boolean)]((id,attr)=>(random_num.nextFloat(),false,true,true))
+    while (remaining_vertices >= 1L) {
+        counter=counter+1
+        var local=local_global.mapVertices[(Float,Boolean,Boolean,Boolean)]((id,attr)=>(random_num.nextFloat(),attr._2,attr._3,attr._4))
+
+        var new_mis=local.aggregateMessages[(Float,Boolean,Boolean,Boolean)](msg=> {
+          if ((msg.srcAttr._1>msg.dstAttr._1) && (msg.srcAttr._3 == true)) {
+            msg.sendToSrc(msg.srcAttr._1,true,msg.srcAttr._3, msg.dstAttr._3)
+          } else if ((msg.srcAttr._1<=msg.dstAttr._1)&&(msg.srcAttr._3==true)&&(msg.dstAttr._3 == true)) {
+            msg.sendToSrc(msg.srcAttr._1,false,msg.srcAttr._3,msg.dstAttr._3)
+          } else {
+            msg.sendToSrc(msg.srcAttr._1,msg.srcAttr._2,msg.srcAttr._3,msg.dstAttr._3)
+          }
+        },
+          (a,b)=>{
+            if (a._4 == false){
+              (b._1,b._2,b._3,b._4)
+            } else if (b._4 == false){
+              (a._1,a._2,a._3,a._4)
+            } else{
+              (a._1,a._2 && b._2,a._3,a._4)
+            }
+          })
+
+        local=Graph(new_mis,local.edges)
+
+        new_mis = local.aggregateMessages[(Float,Boolean,Boolean,Boolean)](msg=> {
+          if ((msg.srcAttr._2 || msg.dstAttr._2) && (msg.srcAttr._3==true)) {
+            msg.sendToSrc(msg.srcAttr._1,msg.srcAttr._2,false,msg.dstAttr._3)
+          } else if ((msg.srcAttr._3==true)) {
+            msg.sendToSrc(msg.srcAttr._1,msg.srcAttr._2,true,msg.dstAttr._3)
+          } else {
+            msg.sendToSrc(msg.srcAttr._1,msg.srcAttr._2,msg.srcAttr._3,msg.dstAttr._3)}
+        },
+          (a,b)=>(a._1,a._2,a._3 && b._3,a._4))
+
+        local=Graph(new_mis,local.edges)
+
+        local_global=local
+
+        remaining_vertices=local_global.vertices.filter{case (id,attr) => attr._3==true}.count
+
+        println("Number of Remaining Vertices: "+remaining_vertices)
     }
+    println(counter)
+    val output=local_global.mapVertices[Int]((id,attr)=>(if (attr._2) {1} else {-1}))
+    return output
   }
 
+  
 
   def verifyMIS(g_in: Graph[Int, Int]): Boolean = {
-    // To Implement
+    val undirectedEdge=g_in.reverse.edges.union(g_in.edges)
+    val undirectedGraph=Graph(g_in.vertices,undirectedEdge)
+    val InGraph=undirectedGraph.subgraph(vpred=(id,label)=>label==1)
+    val OutGraph=undirectedGraph.subgraph(vpred=(id,label)=>label==(-1))
+    if (InGraph.edges.count.toInt==0){
+      val aggregated=undirectedGraph.aggregateMessages[(Int,Int,Int)](msg=>msg.sendToSrc(1,msg.dstAttr,msg.srcAttr),(a,b)=>(a._2+b._2,a._1+b._1,a._3))
+      if (aggregated.filter{ case (element) => (element._2._3==(-1)) && (element._2._1+element._2._2)>1}.count == OutGraph.vertices.count) {
+        return true
+      }
+    }
+    return false
   }
 
 
@@ -45,7 +105,7 @@ object main{
         sys.exit(1)
       }
       val startTimeMillis = System.currentTimeMillis()
-      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
+      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)}).filter{case Edge(a,b,c) => a!=b}
       val g = Graph.fromEdges[Int, Int](edges, 0, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
       val g2 = LubyMIS(g)
 
@@ -64,7 +124,7 @@ object main{
         sys.exit(1)
       }
 
-      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
+      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)}).filter{case Edge(a,b,c) => a!=b}
       val vertices = sc.textFile(args(2)).map(line => {val x = line.split(","); (x(0).toLong, x(1).toInt) })
       val g = Graph[Int, Int](vertices, edges, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
 
